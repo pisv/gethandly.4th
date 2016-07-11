pisv/gethandly.1st

Migrate to Handly 0.5

org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/examples/basic/ui/model/IFooModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 1C LLC.
+ * Copyright (c) 2014, 2016 1C-Soft LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,7 @@ package org.eclipse.handly.examples.basic.ui.model;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.handly.model.IHandle;
+import org.eclipse.handly.model.IElementExtension;
 
 /**
  * Represents the root Foo element corresponding to the workspace. 
@@ -21,7 +21,7 @@ import org.eclipse.handly.model.IHandle;
  * as <em>the</em> Foo Model element.
  */
 public interface IFooModel
-    extends IHandle
+    extends IElementExtension
 {
     /**
      * Returns the Foo project with the given name. The given name must be 

org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/examples/basic/ui/model/IFooProject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 1C LLC.
+ * Copyright (c) 2014, 2016 1C-Soft LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,18 +12,24 @@ package org.eclipse.handly.examples.basic.ui.model;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.handly.internal.examples.basic.ui.model.FooProjectNature;
-import org.eclipse.handly.model.IHandle;
+import org.eclipse.handly.model.IElementExtension;
 
 /**
  * Represents a Foo project.
  */
 public interface IFooProject
-    extends IHandle
+    extends IElementExtension
 {
     /**
      * Foo project nature id.
      */
     String NATURE_ID = FooProjectNature.ID;
+
+    @Override
+    default IFooModel getParent()
+    {
+        return (IFooModel)IElementExtension.super.getParent();
+    }
 
     /**
      * Returns the <code>IProject</code> on which this <code>IFooProject</code>

pisv/gethandly.4th

org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/examples/basic/ui/model/IFooModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 1C LLC.
+ * Copyright (c) 2014, 2016 1C-Soft LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,7 +14,6 @@ import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.handly.model.IElementChangeListener;
-import org.eclipse.handly.model.IHandle;
 
 /**
  * Represents the root Foo element corresponding to the workspace. 
@@ -22,7 +21,7 @@ import org.eclipse.handly.model.IHandle;
  * as <em>the</em> Foo Model element.
  */
 public interface IFooModel
-    extends IHandle, IFooElement
+    extends IFooElement
 {
     /**
      * Adds the given listener for changes to elements in the Foo Model. 

org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/examples/basic/ui/model/IFooProject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 1C LLC.
+ * Copyright (c) 2014, 2016 1C-Soft LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,18 +13,23 @@ package org.eclipse.handly.examples.basic.ui.model;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.handly.internal.examples.basic.ui.model.FooProjectNature;
-import org.eclipse.handly.model.IHandle;
 
 /**
  * Represents a Foo project.
  */
 public interface IFooProject
-    extends IHandle, IFooElement
+    extends IFooElement
 {
     /**
      * Foo project nature id.
      */
     String NATURE_ID = FooProjectNature.ID;
+
+    @Override
+    default IFooModel getParent()
+    {
+        return (IFooModel)IFooElement.super.getParent();
+    }
 
     /**
      * Returns the Foo file with the given name in this project, or 